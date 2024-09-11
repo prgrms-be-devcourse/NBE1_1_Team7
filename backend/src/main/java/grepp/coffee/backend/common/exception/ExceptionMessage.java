0c3a3ec9e602aefb95d3b13f9e38a981888bceb8
ooni <gdbsrjsdn@gmail.com>
2024-09-11 11:29:30 +0900

    cart 관련 Exception 생성

--- backend/src/main/java/grepp/coffee/backend/common/exception/ExceptionMessage.java
+++ backend/src/main/java/grepp/coffee/backend/common/exception/ExceptionMessage.java
@@ -16,7 +16,10 @@ public enum ExceptionMessage {
     ORDER_STATUS_NOT_FENDING("해당 주문은 배송중이거나 완료 상태입니다."),
 
     // Member
-    MEMBER_NOT_FOUND("해당 사용자를 찾을 수 없습니다.")
+    MEMBER_NOT_FOUND("해당 사용자를 찾을 수 없습니다."),
+
+    // Cart
+    CART_NOT_FOUND("해당 장바구니를 찾을 수 없습니다.")
 
     ;
     private final String text;
