fb01d8f8b6f8b10401f900e811257abb102ecefb
ooni <gdbsrjsdn@gmail.com>
2024-09-11 14:48:56 +0900

    Feature: 주문-장바구니 동시처리 로직 수정

--- backend/src/main/java/grepp/coffee/backend/model/service/member/CartService.java
+++ backend/src/main/java/grepp/coffee/backend/model/service/member/CartService.java
@@ -2,14 +2,13 @@ package grepp.coffee.backend.model.service.member;
 
 import grepp.coffee.backend.common.exception.ExceptionMessage;
 import grepp.coffee.backend.common.exception.member.CartException;
-import grepp.coffee.backend.common.exception.member.MemberException;
-import grepp.coffee.backend.common.exception.product.ProductException;
+import grepp.coffee.backend.controller.member.request.CartRegisterRequest;
+import grepp.coffee.backend.controller.member.request.CartUpdateRequest;
 import grepp.coffee.backend.model.entity.cart.Cart;
 import grepp.coffee.backend.model.entity.member.Member;
 import grepp.coffee.backend.model.entity.product.Product;
 import grepp.coffee.backend.model.repository.member.CartRepository;
-import grepp.coffee.backend.model.repository.member.MemberRepository;
-import grepp.coffee.backend.model.repository.product.ProductRepository;
+import grepp.coffee.backend.model.service.product.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -23,28 +22,20 @@ import java.util.List;
 public class CartService {
 
     private final CartRepository cartRepository;
-    private final MemberRepository memberRepository;
-    private final ProductRepository productRepository;
+    private final MemberService memberService;
+    private final ProductService productService;
 
     // 장바구니 추가
     @Transactional
-    public void addCart(Long memberId, Long productId, int quantity) {
-        Member member = memberRepository.findById(memberId)
-                .orElseThrow(() -> {
-                    log.warn(">>>> {} : {} <<<<", memberId, ExceptionMessage.MEMBER_NOT_FOUND);
-                    return new MemberException(ExceptionMessage.MEMBER_NOT_FOUND);
-                });
+    public void addCart(CartRegisterRequest request) {
+        Member member = memberService.findByIdOrThrowMemberException(request.getMemberId());
 
-        Product product = productRepository.findById(productId)
-                .orElseThrow(() -> {
-                    log.warn(">>>> {} : {} <<<<", productId, ExceptionMessage.PRODUCT_NOT_FOUND);
-                    return new ProductException(ExceptionMessage.PRODUCT_NOT_FOUND);
-                });
+        Product product = productService.findByIdOrThrowProductException(request.getProductId());
 
         Cart cart = Cart.builder()
                 .member(member)
                 .product(product)
-                .quantity(quantity)
+                .quantity(request.getQuantity())
                 .build();
 
         cartRepository.save(cart);
@@ -53,24 +44,23 @@ public class CartService {
     // 장바구니 목록 조회
     @Transactional(readOnly = true)
     public List<Cart> getCart(Long memberId) {
-        return cartRepository.findByMemberId(memberId);
+        Member member = memberService.findByIdOrThrowMemberException(memberId);
+
+        return cartRepository.findByMember(member);
     }
 
     // 장바구니 수정
     @Transactional
-    public void updateCart(Long cartId, int quantity) {
-        Cart existingCart = cartRepository.findById(cartId)
-                .orElseThrow(() -> {
-                    log.warn(">>>> {} : {} <<<<", cartId, ExceptionMessage.CART_NOT_FOUND);
-                    return new CartException(ExceptionMessage.CART_NOT_FOUND);
-                });
+    public void updateCart(Long cartId, CartUpdateRequest request) {
+        Cart cart = findByIdOrThrowCartException(cartId);
 
+        Product product = productService.findByIdOrThrowProductException(request.getProductId());
 
         Cart updatedCart = Cart.builder()
-                .cartId(existingCart.getCartId())
-                .member(existingCart.getMember())
-                .product(existingCart.getProduct())
-                .quantity(quantity)
+                .cartId(cart.getCartId())
+                .member(cart.getMember())
+                .product(product)
+                .quantity(request.getQuantity())
                 .build();
 
         cartRepository.save(updatedCart);
@@ -85,6 +75,20 @@ public class CartService {
     // 주문 처리 시, 장바구니 자동 삭제
     @Transactional
     public void deleteCartByOrder(Long memberId, Long productId) {
-        cartRepository.deleteByMemberIdAndProductId(memberId, productId);
+        Member member = memberService.findByIdOrThrowMemberException(memberId);
+        Product product = productService.findByIdOrThrowProductException(productId);
+
+        Cart cart = cartRepository.findByMemberAndProduct(member, product);
+
+        cartRepository.delete(cart);
+    }
+
+    // 장바구니 조회 예외처리
+    public Cart findByIdOrThrowCartException(Long cartId) {
+        return cartRepository.findById(cartId)
+                .orElseThrow(() -> {
+                    log.warn(">>>> {} : {} <<<<", cartId, ExceptionMessage.CART_NOT_FOUND);
+                    return new CartException(ExceptionMessage.CART_NOT_FOUND);
+                });
     }
 }
