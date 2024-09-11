27e99a9e1519b5aab54c1ea77eabf5a9d37e4ec5
ooni <gdbsrjsdn@gmail.com>
2024-09-11 14:48:56 +0900

    Feature: 주문-장바구니 동시처리 로직 수정

--- backend/src/main/java/grepp/coffee/backend/model/repository/member/CartRepository.java
+++ backend/src/main/java/grepp/coffee/backend/model/repository/member/CartRepository.java
@@ -1,6 +1,8 @@
 package grepp.coffee.backend.model.repository.member;
 
 import grepp.coffee.backend.model.entity.cart.Cart;
+import grepp.coffee.backend.model.entity.member.Member;
+import grepp.coffee.backend.model.entity.product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,8 +12,8 @@ import java.util.List;
 public interface CartRepository extends JpaRepository<Cart, Long> {
 
     // 사용자의 장바구니 목록을 조회하는 메서드
-    List<Cart> findByMemberId(Long memberId);
+    List<Cart> findByMember(Member member);
 
-    // 사용자 및 상품에 대안 장바구니 삭제 메서드
-    void deleteByMemberIdAndProductId(Long memberId, Long ProductId);
+    // 멤버 및 상품 가져오는 메서드
+    Cart findByMemberAndProduct(Member member, Product product);
 }
