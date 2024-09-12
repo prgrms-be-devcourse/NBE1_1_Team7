7eb5d1dc5570c80d6325eb26612a51eef85d0ea4
Repaion24 <65394501+Repaion24@users.noreply.github.com>
2024-09-12 12:00:02 +0900

    Feature: QuestionService 수정
    member 유효한지 체크하는 로직 추가

--- backend/src/main/java/grepp/coffee/backend/model/service/question/QuestionService.java
+++ backend/src/main/java/grepp/coffee/backend/model/service/question/QuestionService.java
@@ -5,8 +5,10 @@ import grepp.coffee.backend.common.exception.question.QuestionException;
 import grepp.coffee.backend.controller.question.request.QuestionDeleteRequest;
 import grepp.coffee.backend.controller.question.request.QuestionRegisterRequest;
 import grepp.coffee.backend.controller.question.request.QuestionUpdateRequest;
+import grepp.coffee.backend.model.entity.member.Member;
 import grepp.coffee.backend.model.entity.question.Question;
 import grepp.coffee.backend.model.repository.question.QuestionRepository;
+import grepp.coffee.backend.model.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -20,6 +22,7 @@ import java.util.List;
 public class QuestionService {
 
     private final QuestionRepository questionRepository;
+    private final MemberService memberService;
 
     //FAQ 전체 조회
     public List<Question> readQuestionList() {return questionRepository.findAll();}
@@ -27,8 +30,11 @@ public class QuestionService {
     //질문 등록
     @Transactional
     public void createQuestion(QuestionRegisterRequest request) {
+        // 사용자 조회
+        Member member = memberService.findByIdOrThrowMemberException(request.getMemberId());
+
         Question question = Question.builder()
-                .memberId(request.getMemberId())
+                .memberId(member.getMemberId())
                 .question(request.getQuestion())
                 .answer(request.getAnswer())
                 .build();
@@ -38,10 +44,12 @@ public class QuestionService {
     //질문 수정
     @Transactional
     public void updateQuestion(Long questionId, QuestionUpdateRequest request) {
+        // 사용자 조회
+        Member member = memberService.findByIdOrThrowMemberException(request.getMemberId());
         // 질문 조회
         Question question = findByIdOrThrowQuestionException(questionId);
 
-        if(!request.getMemberId().equals(question.getMemberId())) {
+        if(!member.getMemberId().equals(question.getMemberId())) {
             log.warn(">>>> {} : {} <<<<", questionId, ExceptionMessage.QUESTION_NOT_WRITTEN_BY_MEMBER);
             throw new QuestionException(ExceptionMessage.QUESTION_NOT_WRITTEN_BY_MEMBER);
         }
@@ -57,8 +65,12 @@ public class QuestionService {
     //질문 삭제
     @Transactional
     public void deleteQuestion(Long questionId, QuestionDeleteRequest request) {
+        // 사용자 조회
+        Member member = memberService.findByIdOrThrowMemberException(request.getMemberId());
+        // 질문 조회
         Question question = findByIdOrThrowQuestionException(questionId);
-        if(!request.getMemberId().equals(question.getMemberId())) {
+
+        if(!member.getMemberId().equals(question.getMemberId())) {
             log.warn(">>>> {} : {} <<<<", questionId, ExceptionMessage.QUESTION_NOT_WRITTEN_BY_MEMBER);
             throw new QuestionException(ExceptionMessage.QUESTION_NOT_WRITTEN_BY_MEMBER);
         }
