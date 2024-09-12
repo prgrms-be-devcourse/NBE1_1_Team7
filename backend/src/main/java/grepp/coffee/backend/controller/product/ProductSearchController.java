58bf5560a7f03b1bebbf8e246e8ba4c100f6455e
tnals2384 <tnals2384@gmail.com>
2024-09-12 15:00:52 +0900

    Docs: Swagger description 추가

--- backend/src/main/java/grepp/coffee/backend/controller/product/ProductSearchController.java
+++ backend/src/main/java/grepp/coffee/backend/controller/product/ProductSearchController.java
@@ -2,6 +2,8 @@ package grepp.coffee.backend.controller.product;
 
 import grepp.coffee.backend.model.entity.product.Product;
 import grepp.coffee.backend.model.service.product.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,49 +15,60 @@ import java.util.List;
 @RestController
 @RequestMapping("/search")
 @RequiredArgsConstructor
+@Tag(name = "상품 목록 필터링 API")
 public class ProductSearchController {
 
     private final ProductService productService;
 
-    // 가격 기준으로 오름차순 정렬된 상품 검색
     @GetMapping("/price/asc")
+    @Operation(summary = "정해진 가격 내에서 오름차순 정렬 상품 목록 조회", description = "가격 오름차순으로 상품 목록을 조회하는 API" +
+            "하한 가격과 상한 가격을 param으로 주세요.")
     public List<Product> searchProductsByPriceAsc(
             @RequestParam int minPrice,
             @RequestParam int maxPrice) {
         return productService.searchProductsByPriceAsc(minPrice, maxPrice);
     }
 
-    // 가격 기준으로 내림차순 정렬된 상품 검색
+
     @GetMapping("/price/desc")
+    @Operation(summary = "정해진 가격 내에서 내림차순 정렬 상품 목록 조회", description = "가격 내림차순으로 상품 목록을 조회하는 API" +
+            "하한 가격과 상한 가격을 param으로 주세요.")
     public List<Product> searchProductsByPriceDesc(
             @RequestParam int minPrice,
             @RequestParam int maxPrice) {
         return productService.searchProductsByPriceDesc(minPrice, maxPrice);
     }
 
-    // 가격없이 정렬 기준에 따라서만 정렬 true -> 오름차순 || false -> 내림차순
+
     @GetMapping("/price")
+    @Operation(summary = "가격을 기준으로 정렬하여 상품 목록 조회", description = "가격 오름차순, 내림차순으로 상품목록을 조회하는 API" +
+            "param값이 true면 오름차순, false면 내림차순입니다.")
     public List<Product> searchProductsByPrice(
             @RequestParam(defaultValue = "true") boolean ascending) {
         return productService.searchProductsByPrice(ascending);
     }
 
-    // 주문량에 따라 정렬 true -> 오름차순 || false -> 내림차순
+
     @GetMapping("/order-count")
+    @Operation(summary = "주문량을 기준으로 정렬하여 상품 목록 조회", description = "주문량 오름차순, 내림차순으로 상품목록을 조회하는 API" +
+            "param값이 true면 오름차순, false면 내림차순입니다.")
     public List<Product> searchProductsByOrderCount(
             @RequestParam(defaultValue = "true") boolean ascending) {
         return productService.searchProductsByOrderCount(ascending);
     }
 
-    // 이름순으로 정렬 true -> 오름차순 || false -> 내림차순
+
     @GetMapping("/name")
+    @Operation(summary = "이름을 기준으로 정렬하여 상품 목록 조회", description = "이름 오름차순, 내림차순으로 상품목록을 조회하는 API" +
+            "param값이 true면 오름차순, false면 내림차순입니다.")
     public List<Product> searchProductsByName(
             @RequestParam(defaultValue = "true") boolean ascending) {
         return productService.searchProductsByName(ascending);
     }
 
-    // 별점 평균 순으로 정렬 true -> 오름차순 || false -> 내림차순
     @GetMapping("/rating")
+    @Operation(summary = "별점을 기준으로 정렬하여 상품 목록 조회", description = "별점 오름차순, 내림차순으로 상품목록을 조회하는 API" +
+            "param값이 true면 오름차순, false면 내림차순입니다.")
     public List<Product> searchProductsByRating(
             @RequestParam(defaultValue = "true") boolean ascending) {
         return productService.searchProductsByRating(ascending);
--- backend/src/main/java/grepp/coffee/backend/controller/question/QuestionController.java
+++ backend/src/main/java/grepp/coffee/backend/controller/question/QuestionController.java
@@ -6,6 +6,7 @@ import grepp.coffee.backend.controller.question.request.QuestionUpdateRequest;
 import grepp.coffee.backend.model.entity.question.Question;
 import grepp.coffee.backend.model.service.question.QuestionService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,27 +19,25 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/question")
 @RestController
+@Tag(name = "FAQ API")
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    // FAQ 목록
     @Operation(summary = "FAQ 목록 조회", description = "모든 FAQ를 조회하는 API")
     @GetMapping("")
     public ResponseEntity<List<Question>> getAllQuestions() {
         return ResponseEntity.ok().body(questionService.readQuestionList());
     }
 
-    // FAQ 등록
-    @Operation(summary = "FAQ 등록", description = "새로운 질문과 답변(QNA)을 등록하는 API")
+    @Operation(summary = "[관리자] FAQ 등록", description = "관리자가 새로운 질문과 답변(QNA)을 등록하는 API")
     @PostMapping("/")
     public ResponseEntity<Void> createQuestion(@Valid @RequestBody QuestionRegisterRequest question) {
         questionService.createQuestion(question);
         return ResponseEntity.ok().build();
     }
 
-    // FAQ 수정
-    @Operation(summary = "FAQ 수정", description = "특정 질문과 답변(FAQ)을 수정하는 API")
+    @Operation(summary = "[관리자] FAQ 수정", description = "관리자가 특정 질문과 답변(FAQ)을 수정하는 API")
     @PutMapping("/{questionId}")
     public ResponseEntity<Void> updateQuestion(@PathVariable(name = "questionId") Long questionId,
                                                @Valid @RequestBody QuestionUpdateRequest question){
@@ -47,8 +46,7 @@ public class QuestionController {
     }
 
 
-    // FAQ 삭제
-    @Operation(summary = "FAQ 삭제", description = "특정 질문과 답변(FAQ)을 삭제하는 API")
+    @Operation(summary = "[관리자] FAQ 삭제", description = "관리자가 특정 질문과 답변(FAQ)을 삭제하는 API")
     @DeleteMapping("/{questionId}")
     public ResponseEntity<Void> deleteQuestion(@PathVariable(name = "questionId") Long questionId,
                                                @Valid @RequestBody QuestionDeleteRequest question) {
